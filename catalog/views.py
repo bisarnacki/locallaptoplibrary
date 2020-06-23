56c63684d9e3149e1e40fe56d95c28cb264b3bac
bisarnacki <this.igorovski@pm.me>
2020-06-23 12:49:43 -0400

    updated base_generic.html and created detail view for item instances

--- catalog/views.py
+++ catalog/views.py
@@ -33,3 +33,6 @@ class BookListView(generic.ListView):
     context_object_name = 'my_book_list'   # your own name for the list as a template variable
     queryset = Book.objects.filter(title__icontains='war')[:5] # Get 5 books containing the title war
     template_name = 'books/my_arbitrary_template_name_list.html'  # Specify your own template name/location"""
+
+    def get_queryset(self):
+        return Book.objects.filter(title__icontains='war')[:5] # Get 5 books containing the title war
