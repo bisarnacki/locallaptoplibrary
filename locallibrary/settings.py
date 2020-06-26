a9a7f855ee5f21d5041494a1c0088ee5722f52d3
bisarnacki <this.igorovski@pm.me>
2020-06-26 07:02:56 -0400

    updated settings.py

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -121,3 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Updating db/sending cookie on every request 
+
+SESSION_SAVE_EVERY_REQUEST = True
